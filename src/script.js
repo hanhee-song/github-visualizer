235fc401bd9fbbcfbe3527c96abcc833a7513e3a
Hanhee Song <song.hanhee@gmail.com>
2017-11-08 19:20:49 -0500

    setup data retrieval

--- src/script.js
+++ src/script.js
@@ -3,60 +3,53 @@ const svg = d3.select('.svg-main');
 const width = Number(svg.attr("width"));
 const height = Number(svg.attr("height"));
 
-const nodes = [
-  {"id": "asdf1"},
-  {"id": "asdf2"}
-];
-
-const links = [
-  { source: "asdf1", target: "asdf2" }
-];
-
-
 const simulation = d3.forceSimulation()
   .force("link", d3.forceLink().id((d) => d.id))
   .force("charge", d3.forceManyBody())
-  // .force("center", d3.forceCenter(width/2, height/2))
   .force("radial", d3.forceRadial(100, width/2, height/2));
 
 //
 
-const link = svg.append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(links)
-  .enter().append("line")
-    .attr("stroke-width", (d) => Math.sqrt(d.value));
+d3.json("../data/filetree.json", function(error, graph) {
+  if (error) throw error;
+  
+  const link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
 
-const node = svg.append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .data(nodes)
-  .enter().append("circle")
-    .attr("r", 5)
-    .call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended)
-    );
+  const node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", 5)
+      .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+      );
 
-simulation.nodes(nodes)
-  .on("tick", ticked);
+  simulation.nodes(graph.nodes)
+    .on("tick", ticked);
 
-simulation.force("link")
-  .links(links);
-  // .on("tick", ticked); // ?
+  simulation.force("link")
+    .links(graph.links);
+    // .on("tick", ticked); // ?
 
-function ticked() {
-  link
-    .attr("x1", (d) => d.source.x)
-    .attr("y1", (d) => d.source.y)
-    .attr("x2", (d) => d.target.x)
-    .attr("y2", (d) => d.target.y);
-  node
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y);
-}
+  function ticked() {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+    node
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y);
+  }
+});
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
