8c7d3df30c9948e820861c51afcf5a586e659c6b
Hanhee Song <song.hanhee@gmail.com>
2018-03-23 00:22:59 -0400

    add url parsing functions

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -17,13 +17,42 @@ export class SidebarComponent implements OnInit {
   }
   
   handleChange(e) {
-    
+    this.makeUrl()
   }
   
   handleUrlChange(e) {
+    this.parseUrl()
+  }
+  
+  handleSubmit() {
     
   }
-
+  
+  makeUrl() {
+    let url = `https://github.com/${this.user}`;
+    if (this.repo) {
+      url += '/' + this.repo;
+    }
+    if (this.subdir) {
+      url += '/tree/master/' + this.subdir;
+    }
+    this.url = url;
+  }
+  
+  parseUrl() {
+    let urlTag = this.url.match(/github\.com\/(.*)/);
+    if (urlTag) {
+      const urlTagArr = urlTag[1].split("/");
+      this.user = urlTagArr[0] ? urlTagArr[0] : "";
+      this.repo = urlTagArr[1] ? urlTagArr[1] : "";
+      if (urlTagArr[2] === "tree" && urlTagArr[3] === "master"
+        && urlTagArr[4]) {
+        this.subdir = urlTagArr.slice(4).join("/");
+      } else {
+        this.subdir = "";
+      }
+    }
+  }
 }
 
 
