f66cb404854844d9cb2698496c2860c70b7d55c6
Hanhee Song <song.hanhee@gmail.com>
2018-03-24 17:34:57 -0400

    I refactored a ton of stuff and it still works

--- src/app/d3-container/d3-container.component.ts
+++ src/app/d3-container/d3-container.component.ts
@@ -1,3 +1,4 @@
+import { SidebarContentService } from './../sidebar-content.service';
 import { GitApiService } from './../git-api.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,12 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./d3-container.component.css']
 })
 export class D3ContainerComponent implements OnInit {
+  graph
+  link
+  node
+  text
+  width
+  height
+  simulation
+  highlightedId = "";
+  hoveredId = "";
+  clickedId = "";
+  searchedId = "";
+  frozen = false;
+  linkedById = {}
 
   constructor(
     private gitApiService: GitApiService
   ) {
     this.gitApiService.graphChange.subscribe(graph => {
-      this.drawGraph(graph, "", "", "")
+      this.drawGraph(graph)
     })
   }
 
@@ -24,27 +38,20 @@ export class D3ContainerComponent implements OnInit {
   // There's a good deal of really messy code from back when this was
   // executed functional-style in a separate file
   
-  drawGraph(graph, user, repo, subdir) {
-    // generateHeader(graph, user, repo, subdir);
-    // setContentMessage();
-
+  drawGraph(graph) {
+    this.graph = graph
     const svg = d3.select('.svg-main');
     svg.selectAll("g").remove();
     svg.selectAll("text").remove();
-    const width = Number(svg.attr("width"));
-    const height = Number(svg.attr("height"));
-    let highlightedId = "";
-    let hoveredId = "";
-    let clickedId = "";
-    let searchedId = "";
-    let frozen = false;
+    this.width = Number(svg.attr("width"));
+    this.height = Number(svg.attr("height"));
 
-    svg.on("click", unhighlightNode);
+    svg.on("click", this.unhighlightNode.bind(this));
 
-    const simulation = d3.forceSimulation()
+    this.simulation = d3.forceSimulation()
       .force(
         "link", d3.forceLink()
-          .distance(d => distance(d))
+          .distance(d => this.distance(d))
           .strength(1)
           .id((d) => d.id)
       )
@@ -53,7 +60,7 @@ export class D3ContainerComponent implements OnInit {
 
     // LINKS, NODES, AND TEXT =============================
 
-    const link = svg.append("g")
+    this.link = svg.append("g")
       .attr("class", "links")
       .selectAll("line")
       .data(graph.links)
@@ -84,242 +91,46 @@ export class D3ContainerComponent implements OnInit {
       ));
     }
 
-    const node = svg.append("g")
+    this.node = svg.append("g")
       .attr("class", "nodes")
       .selectAll("circle")
       .data(graphNodes)
       .enter()
       .append("circle")
       .attr("r", d => d.r)
-      .attr("fill", d => color(d))
+      .attr("fill", d => this.color(d))
       .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended)
+        .on("start", this.dragstarted.bind(this))
+        .on("drag", this.dragged.bind(this))
+        .on("end", this.dragended.bind(this))
       )
-      .on("click", highlightNode)
-      .on("mouseover", hoverNode)
-      .on("mouseout", hoverNode);
+      .on("click", this.highlightNode.bind(this))
+      .on("mouseover", this.hoverNode.bind(this))
+      .on("mouseout", this.hoverNode.bind(this));
 
-    const text = svg.selectAll("text").filter(".label")
+    this.text = svg.selectAll("text").filter(".label")
       .data(graphNodes)
       .enter()
       .append("text")
       .classed("label", true)
-      .text((d) => abbreviate(d.name));
+      .text((d) => this.abbreviate(d.name));
 
-    simulation.nodes(graphNodes)
-      .on("tick", ticked);
+    this.simulation.nodes(graphNodes)
+      .on("tick", this.ticked.bind(this));
 
-    simulation.force("link")
+    this.simulation.force("link")
       .links(graph.links);
-
-    // TICK FUNCTION ========================================
-
-    function ticked() {
-      const boundedX = (d) => {
-        return Math.max(d.r, Math.min(width - d.r - 2, d.x));
-      };
-      const boundedY = (d) => {
-        return Math.max(d.r, Math.min(height - d.r - 2, d.y));
-      };
-
-      const circumferenceHash = {};
-      link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => {
-          circumferenceHash[d.source.id + d.target.id] = getCircumferencePoint(d);
-          return circumferenceHash[d.source.id + d.target.id][0];
-        })
-        .attr("y2", (d) => circumferenceHash[d.source.id + d.target.id][1]);
-
-      node
-        .attr("cx", (d) => {
-          d.x = boundedX(d);
-          return d.x;
-        })
-        .attr("cy", (d) => {
-          d.y = boundedY(d);
-          return d.y;
-        });
-
-      text
-        .attr("x", (d) => d.x + 1 + d.r)
-        .attr("y", (d) => d.y + 3);
-
-      function getCircumferencePoint(d) {
-        const tRadius = d.target.r;
-        const dx = d.target.x - d.source.x;
-        const dy = d.target.y - d.source.y;
-        const gamma = Math.atan2(dy, dx);
-        const tx = d.target.x - (Math.cos(gamma) * tRadius);
-        const ty = d.target.y - (Math.sin(gamma) * tRadius);
-        return [tx, ty];
-      }
-    }
-
+      
     // NEIGHBOR HELPER METHODS ===========================
 
-    const linkedById = {};
+    this.linkedById = {};
     for (let i = 0; i < graphNodes.length; i++) {
-      linkedById[`${i},${i}`] = 1;
+      this.linkedById[`${i},${i}`] = 1;
     }
     graph.links.forEach(d => {
-      linkedById[`${d.source.id},${d.target.id}`] = 1;
+      this.linkedById[`${d.source.id},${d.target.id}`] = 1;
     });
 
-    function linkedIds(a, b) {
-      return linkedById[`${a},${b}`];
-    }
-
-    function adjacent(o, d) {
-      const oId = o.id ? o.id : o;
-      const dId = d.id ? d.id : d;
-      return linkedIds(oId, dId) || linkedIds(dId, oId) || oId === dId;
-    }
-
-    function relatedSearch(d) {
-      if (!searchedId) {
-        return false;
-      }
-
-      let name = d.name.toLowerCase();
-      let query = searchedId.toLowerCase().split(" ");
-      for (let i = 0; i < query.length; i++) {
-        if (!name.includes(query[i])) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    // GENERATE TEXT / OPACITY ===========================
-
-    function generateText() {
-      const mousedId = clickedId || hoveredId;
-      text.text((o) => {
-        if (highlightedId && adjacent(highlightedId, o)
-          || mousedId && adjacent(o, mousedId)
-          || relatedSearch(o)) {
-          return unextended(o.name);
-        } else {
-          return abbreviate(o.name);
-        }
-      });
-    }
-
-    function generateOpacity() {
-      let opacity;
-      if (highlightedId) {
-        opacity = .1;
-      } else if (hoveredId || clickedId) {
-        opacity = .3;
-      } else {
-        opacity = searchedId ? .5 : 1;
-      }
-      const partialOpacity = .5;
-      const linkFactor = .6;
-      const mousedId = clickedId || hoveredId;
-
-      node.style("opacity", (o) => {
-        if (relatedSearch(o)) {
-          return 1;
-        } else if (adjacent(o, highlightedId)) {
-          return 1;
-        } else if (adjacent(o, mousedId)) {
-          return highlightedId ? partialOpacity : 1;
-        } else {
-          return opacity;
-        }
-      });
-      node.style("stroke", (o) => {
-        if (o.id === highlightedId) {
-          return "#ccf";
-        } else if (o.id === mousedId) {
-          return "#99c";
-        } else if (relatedSearch(o)) {
-          return "#7b7";
-        } else {
-          return "#232b42";
-        }
-      });
-      link.style("opacity", (o) => {
-        if (highlightedId === o.source.id || highlightedId === o.target.id) {
-          return linkFactor;
-        } else if (mousedId === o.source.id || mousedId === o.target.id) {
-          return highlightedId ? linkFactor * .7 : linkFactor;
-        } else {
-          return linkFactor * opacity;
-        }
-      });
-      text.style("opacity", (o) => {
-        if (relatedSearch(o) || adjacent(o, highlightedId)) {
-          return 1;
-        } else if (adjacent(o, mousedId)) {
-          return highlightedId ? partialOpacity : 1;
-        } else {
-          return opacity;
-        }
-      });
-    }
-
-    // HOVER / HIGHLIGHT / CLICK EFFECTS ===================
-
-    function highlightNode(d) {
-      d3.event.stopPropagation();
-      if (!highlightedId || highlightedId !== d.id) {
-        highlightedId = d.id;
-        // setContentMessage(d.content);
-        // generateHeader(graph, user, repo, subdir, d);
-      } else {
-        // setContentMessage();
-        // generateHeader(graph, user, repo, subdir);
-        highlightedId = "";
-      }
-      generateOpacity();
-      generateText();
-    }
-
-    function unhighlightNode(d) {
-      // setContentMessage();
-      // generateHeader(graph, user, repo, subdir);
-      highlightedId = "";
-      generateOpacity();
-      generateText();
-    }
-
-    function hoverNode(d) {
-      if (hoveredId !== d.id) {
-        hoveredId = d.id;
-      } else {
-        hoveredId = "";
-      }
-      generateOpacity();
-      generateText();
-    }
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      clickedId = d.id;
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      clickedId = "";
-      generateOpacity();
-      generateText();
-      if (!frozen) {
-        d.fx = null;
-        d.fy = null;
-      }
-    }
 
     // EVENT LISTENERS - SEARCH / PAUSE ====================
 
@@ -373,48 +184,244 @@ export class D3ContainerComponent implements OnInit {
     //     });
     //   }
     // }
+  }
+  
+  // TICK FUNCTION ========================================
+  
+  _boundedX(d) {
+    return Math.max(d.r, Math.min(this.width - d.r - 2, d.x));
+  };
+  _boundedY(d) {
+    return Math.max(d.r, Math.min(this.height - d.r - 2, d.y));
+  };
+  
+  ticked() {
+    const circumferenceHash = {};
+    this.link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => {
+        circumferenceHash[d.source.id + d.target.id] = this.getCircumferencePoint(d);
+        return circumferenceHash[d.source.id + d.target.id][0];
+      })
+      .attr("y2", (d) => circumferenceHash[d.source.id + d.target.id][1]);
+
+    this.node
+      .attr("cx", (d) => {
+        d.x = this._boundedX(d);
+        return d.x;
+      })
+      .attr("cy", (d) => {
+        d.y = this._boundedY(d);
+        return d.y;
+      });
+
+    this.text
+      .attr("x", (d) => d.x + 1 + d.r)
+      .attr("y", (d) => d.y + 3);
+  }
+  
+  getCircumferencePoint(d) {
+    const tRadius = d.target.r;
+    const dx = d.target.x - d.source.x;
+    const dy = d.target.y - d.source.y;
+    const gamma = Math.atan2(dy, dx);
+    const tx = d.target.x - (Math.cos(gamma) * tRadius);
+    const ty = d.target.y - (Math.sin(gamma) * tRadius);
+    return [tx, ty];
+  }
+  
+  linkedIds(a, b) {
+    return this.linkedById[`${a},${b}`];
+  }
+  
+  adjacent(o, d) {
+    const oId = o.id ? o.id : o;
+    const dId = d.id ? d.id : d;
+    return this.linkedIds(oId, dId) || this.linkedIds(dId, oId) || oId === dId;
+  }
+  
+  // HOVER / HIGHLIGHT / CLICK EFFECTS ===================
+  
+  highlightNode(d) {
+    d3.event.stopPropagation();
+    if (!this.highlightedId || this.highlightedId !== d.id) {
+      this.highlightedId = d.id;
+      // setContentMessage(d.content);
+      // generateHeader(graph, user, repo, subdir, d);
+    } else {
+      // setContentMessage();
+      // generateHeader(graph, user, repo, subdir);
+      this.highlightedId = "";
+    }
+    this.generateOpacity();
+    this.generateText();
+  }
+  
+  unhighlightNode(d) {
+    // setContentMessage();
+    // generateHeader(graph, user, repo, subdir);
+    this.highlightedId = "";
+    this.generateOpacity();
+    this.generateText();
+  }
+  
+  hoverNode(d) {
+    if (this.hoveredId !== d.id) {
+      this.hoveredId = d.id;
+    } else {
+      this.hoveredId = "";
+    }
+    this.generateOpacity();
+    this.generateText();
+  }
+
+  dragstarted(d) {
+    if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+    this.clickedId = d.id;
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  dragended(d) {
+    this.clickedId = "";
+    this.generateOpacity();
+    this.generateText();
+    if (!this.frozen) {
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+  
+  // GENERATE TEXT / OPACITY ===========================
+  
+  generateText() {
+    const mousedId = this.clickedId || this.hoveredId;
+    this.text.text((o) => {
+      if (this.highlightedId && this.adjacent(this.highlightedId, o)
+        || mousedId && this.adjacent(o, mousedId)
+        || this.relatedSearch(o)) {
+        return this.unextended(o.name);
+      } else {
+        return this.abbreviate(o.name);
+      }
+    });
+  }
+  
+  generateOpacity() {
+    let opacity;
+    if (this.highlightedId) {
+      opacity = .1;
+    } else if (this.hoveredId || this.clickedId) {
+      opacity = .3;
+    } else {
+      opacity = this.searchedId ? .5 : 1;
+    }
+    const partialOpacity = .5;
+    const linkFactor = .6;
+    const mousedId = this.clickedId || this.hoveredId;
+
+    this.node.style("opacity", (o) => {
+      if (this.relatedSearch(o)) {
+        return 1;
+      } else if (this.adjacent(o, this.highlightedId)) {
+        return 1;
+      } else if (this.adjacent(o, mousedId)) {
+        return this.highlightedId ? partialOpacity : 1;
+      } else {
+        return opacity;
+      }
+    });
+    this.node.style("stroke", (o) => {
+      if (o.id === this.highlightedId) {
+        return "#ccf";
+      } else if (o.id === mousedId) {
+        return "#99c";
+      } else if (this.relatedSearch(o)) {
+        return "#7b7";
+      } else {
+        return "#232b42";
+      }
+    });
+    this.link.style("opacity", (o) => {
+      if (this.highlightedId === o.source.id || this.highlightedId === o.target.id) {
+        return linkFactor;
+      } else if (mousedId === o.source.id || mousedId === o.target.id) {
+        return this.highlightedId ? linkFactor * .7 : linkFactor;
+      } else {
+        return linkFactor * opacity;
+      }
+    });
+    this.text.style("opacity", (o) => {
+      if (this.relatedSearch(o) || this.adjacent(o, this.highlightedId)) {
+        return 1;
+      } else if (this.adjacent(o, mousedId)) {
+        return this.highlightedId ? partialOpacity : 1;
+      } else {
+        return opacity;
+      }
+    });
+  }
+  
+  relatedSearch(d) {
+    if (!this.searchedId) {
+      return false;
+    }
+
+    let name = d.name.toLowerCase();
+    let query = this.searchedId.toLowerCase().split(" ");
+    for (let i = 0; i < query.length; i++) {
+      if (!name.includes(query[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
 
     // MISC HELPER METHODS ============================
 
-    function color(d) {
-      const group = d.group;
-      const colors = [
-        "#9e6bba",
-        "#309b55",
-        "#4286f4",
-        "#6a7384",
-        "#844040",
-        "#c49c25"
-      ];
-      return colors[group % 6];
-    }
+  color(d) {
+    const group = d.group;
+    const colors = [
+      "#9e6bba",
+      "#309b55",
+      "#4286f4",
+      "#6a7384",
+      "#844040",
+      "#c49c25"
+    ];
+    return colors[group % 6];
+  }
 
-    function abbreviate(name) {
-      let abb;
-      const extensionless = name.match(/(.*)\.js/)[1];
-      const split = extensionless.split(/[-_\.]/);
-      if (split.length > 1) {
-        return split.map(word => word[0]).join("");
-      } else {
-        return name[0] + extensionless.slice(1).split(/[a-z]/).join("");
-      }
-    }
-
-    function unextended(name) {
-      return name.match(/^(.*)\.[A-Za-z]+$/)[1];
-    }
-
-    function distance(d) {
-      const offset = d.target.r + d.source.r;
-      const containerless = (name) => name.split(/(_container)|\./)[0];
-      if (containerless(d.source.id) === containerless(d.target.id)) {
-        return 60 + offset;
-      }
-      let distanceDenom = 200 + Math.min(graph.nodes.length * 2, 250);
-
-      return 30000 / distanceDenom + offset;
+  abbreviate(name) {
+    let abb;
+    const extensionless = name.match(/(.*)\.js/)[1];
+    const split = extensionless.split(/[-_\.]/);
+    if (split.length > 1) {
+      return split.map(word => word[0]).join("");
+    } else {
+      return name[0] + extensionless.slice(1).split(/[a-z]/).join("");
     }
   }
 
+  unextended(name) {
+    return name.match(/^(.*)\.[A-Za-z]+$/)[1];
+  }
 
+  distance(d) {
+    const offset = d.target.r + d.source.r;
+    const containerless = (name) => name.split(/(_container)|\./)[0];
+    if (containerless(d.source.id) === containerless(d.target.id)) {
+      return 60 + offset;
+    }
+    let distanceDenom = 200 + Math.min(this.graph.nodes.length * 2, 250);
+
+    return 30000 / distanceDenom + offset;
+  }
 }
